3461f65826c5a2c3f0235594dcbbf2e51b663c3c
antons.filipovs <antons.filipovs@gmail.com>
2026-03-03 12:44:27 +0200

    docker-compose update

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,6 +4,6 @@ export * from './login/Login';
 export * from './room/Room';
 export * from './reservations/Reservations';
 export * from './occupancy/Occupancy';
-export * from './Error/Error';
+export * from './error/Error';
 export * from './admin-dashboard/AdminDashboard';
 export * from './add-room/AddRoom';
